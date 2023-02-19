e84e3fca3046e1404948fc1f090a71fd540d12bf
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2023-02-19 17:37:50 +0100

    initial commit

---
+++ lib/shoplex.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "shoplex/version"
+
+module Shoplex
+  class Error < StandardError; end
+
+  def self.process file_content
+    return "\n"
+  end
+end
---
+++ test/test_shoplex.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestShoplex < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Shoplex::VERSION
+  end
+
+  def test_it_ignores_lines_without_invoice_number
+    result_file_content = Shoplex::process(File.read('test/files/two_lines_one_invoice_shopware.csv'))
+    assert_equal 1, result_file_content.lines.count
+  end
+end

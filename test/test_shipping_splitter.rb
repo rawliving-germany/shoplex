2e344871dcf443007653935a2c27d9abeee695d3
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2023-03-02 17:38:50 +0100

    rename test cases

--- test/test_shipping_splitter.rb
+++ test/test_shipping_splitter.rb
@@ -3,7 +3,7 @@
 require "test_helper"
 
 class TestShippingSlitter < Minitest::Test
-  def test_easy_19_case
+  def test_it_adds_19_percent_of_shipping_if_all_articles_are_19_percent
     invoice = Shoplex::ShopwareInvoice.new(invoice_amount: 20,
                                            shipping_gross: 10,
                                            tax19_amount: 1.9)
@@ -13,8 +13,8 @@ class TestShippingSlitter < Minitest::Test
     assert_equal 0.0      , invoice.tax07_amount
     assert_equal 1.9 + 1.9, invoice.tax19_amount
   end
-  def test_easy_07_case
-    # if all os taxed 7%, shipping costs are too
+
+  def test_it_adds_07_percent_of_shipping_if_all_articles_are_07_percent
     invoice = Shoplex::ShopwareInvoice.new(invoice_amount: 20,
                                            shipping_gross: 10,
                                            tax07_amount: 0.7)
@@ -24,6 +24,7 @@ class TestShippingSlitter < Minitest::Test
     assert_equal 0.7 + 0.7, invoice.tax07_amount
     assert_equal 0.0      , invoice.tax19_amount
   end
+
   def test_5050_case
     invoice = Shoplex::ShopwareInvoice.new(invoice_amount: 40,
                                            shipping_gross: 20,

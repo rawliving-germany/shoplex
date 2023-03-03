f59e998cd472683d2051de6415d59462a83f1a70
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2023-03-03 22:11:00 +0100

    raise sanity exception with more info

--- lib/shoplex/sanity_check.rb
+++ lib/shoplex/sanity_check.rb
@@ -2,8 +2,13 @@ module Shoplex
   class SanityCheck
     class GrossDoesNotMatchNet < StandardError ; end
     def self.check!(invoice:)
-      if invoice.invoice_amount != invoice.invoice_amount_net + invoice.tax07_amount + invoice.tax19_amount
-        raise GrossDoesNotMatchNet
+      net_tax_sum = (invoice.invoice_amount_net + invoice.tax07_amount + invoice.tax19_amount).round(2)
+      if invoice.invoice_amount != net_tax_sum
+        raise GrossDoesNotMatchNet.new(
+          "Gross amount #{invoice.invoice_amount} does not equal net + tax sums "\
+          "(#{invoice.invoice_amount_net} "\
+          "+ #{invoice.tax07_amount} "\
+          "+ #{invoice.tax19_amount} == #{net_tax_sum})")
       end
     end
   end

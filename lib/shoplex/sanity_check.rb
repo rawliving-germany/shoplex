9ecf4d5c382c55ec673c136a7dad69205b1e212d
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2023-03-11 12:49:43 +0100

    adjust sanity_chevker to gross values

--- lib/shoplex/sanity_check.rb
+++ lib/shoplex/sanity_check.rb
@@ -2,13 +2,12 @@ module Shoplex
   class SanityCheck
     class GrossDoesNotMatchNet < StandardError ; end
     def self.check!(invoice:)
-      net_tax_sum = (invoice.invoice_amount_net + invoice.tax07_amount + invoice.tax19_amount).round(2)
-      if invoice.invoice_amount != net_tax_sum
+      gross_tax_sum = (invoice.tax07_amount + invoice.tax19_amount).round(2) + invoice.shipping_net
+      if invoice.invoice_amount != gross_tax_sum
         raise GrossDoesNotMatchNet.new(
-          "Gross amount #{invoice.invoice_amount} does not equal net + tax sums "\
-          "(#{invoice.invoice_amount_net} "\
-          "+ #{invoice.tax07_amount} "\
-          "+ #{invoice.tax19_amount} == #{net_tax_sum})")
+          "Gross amount #{invoice.invoice_amount} does not equal tax sums "\
+          "(#{invoice.tax07_amount} "\
+          "+ #{invoice.tax19_amount} == #{gross_tax_sum})")
       end
     end
   end

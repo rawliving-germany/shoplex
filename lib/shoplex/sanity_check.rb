5a729e6656b8df59ab0e8a64bfb82901b1bbaaf6
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2023-03-11 18:26:51 +0100

    improve sanity check

--- lib/shoplex/sanity_check.rb
+++ lib/shoplex/sanity_check.rb
@@ -1,14 +1,19 @@
 module Shoplex
   class SanityCheck
     class GrossDoesNotMatchNet < StandardError ; end
+    TOLERANCE = 0.01
     def self.check!(invoice:)
-      gross_tax_sum = (invoice.tax07_amount + invoice.tax19_amount).round(2) + invoice.shipping_net
-      if invoice.invoice_amount != gross_tax_sum
+      gross_tax_sum = (invoice.tax07_amount + invoice.tax19_amount) + invoice.shipping_net
+      if absdiff(invoice.invoice_amount, gross_tax_sum) >= TOLERANCE
         raise GrossDoesNotMatchNet.new(
           "Gross amount #{invoice.invoice_amount} does not equal tax sums "\
           "(#{invoice.tax07_amount} "\
           "+ #{invoice.tax19_amount} == #{gross_tax_sum})")
       end
+    end
+
+    def self.absdiff a, b
+      (a-b).abs
     end
   end
 end
